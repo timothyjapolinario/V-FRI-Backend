55e1f71a6f75e8d133c855b2cede115406238596
timothyjapolinario <timothyjapolinario@gmail.com>
2023-11-17 21:18:24 +0800

    Update scripts

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT ? process.env.PORT : 3001;
+const port = 3000;
 app.get("/", (req, res) => {
     res.send("Express + TypeScript Server");
 });
